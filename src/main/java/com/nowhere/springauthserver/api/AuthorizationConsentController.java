46e6a07b950ac2c0010b8361e256fb2ea438ce35
PUBLISH_TOKEN <rodrigo.bau.fon@gmail.com>
2024-02-15 00:13:23 +0000

    [S1-C5] refactor

--- src/main/java/com/nowhere/springauthserver/api/AuthorizationConsentController.java
+++ src/main/java/com/nowhere/springauthserver/api/AuthorizationConsentController.java
@@ -1,7 +1,5 @@
 package com.nowhere.springauthserver.api;
 
-import org.springframework.web.bind.annotation.RestController;
-
 import java.security.Principal;
 import java.util.Collections;
 import java.util.HashMap;
@@ -38,47 +36,50 @@ public class AuthorizationConsentController {
     @GetMapping("/oauth2/consent")
     public String consent(Principal principal, Model model,
                           @RequestParam(OAuth2ParameterNames.CLIENT_ID) String clientId,
-                          @RequestParam(OAuth2ParameterNames.SCOPE) String scope,
+                          @RequestParam(OAuth2ParameterNames.SCOPE) String scopes,
                           @RequestParam(OAuth2ParameterNames.STATE) String state,
                           @RequestParam(name = OAuth2ParameterNames.USER_CODE, required = false) String userCode) {
 
-        // Remove scopes that were already approved
+        RegisteredClient registeredClient = this.registeredClientRepository.findByClientId(clientId);
+        OAuth2AuthorizationConsent currentAuthConsent =
+                this.authorizationConsentService.findById(registeredClient.getId(), principal.getName());
+
+        // Calculate scopes
+        Map<String, Set<String>> scopeMap = calculateScopes(currentAuthConsent, scopes);
+
+        model.addAttribute("clientId", clientId);
+        model.addAttribute("clientName", registeredClient.getClientName());
+        model.addAttribute("state", state);
+        model.addAttribute("scopes", withDescription(scopeMap.get("scopesToApprove")));
+        model.addAttribute("previouslyApprovedScopes", withDescription(scopeMap.get("previouslyApprovedScopes")));
+        model.addAttribute("principalName", principal.getName());
+        model.addAttribute("requestURI", "/oauth2/authorize");
+        model.addAttribute("userCode", userCode);
+        return "consent";
+    }
+
+    private Map<String, Set<String>> calculateScopes(OAuth2AuthorizationConsent currentAuthConsent, String scopes) {
         Set<String> scopesToApprove = new HashSet<>();
         Set<String> previouslyApprovedScopes = new HashSet<>();
-        RegisteredClient registeredClient = this.registeredClientRepository.findByClientId(clientId);
-        OAuth2AuthorizationConsent currentAuthorizationConsent =
-                this.authorizationConsentService.findById(registeredClient.getId(), principal.getName());
-        Set<String> authorizedScopes;
-        if (currentAuthorizationConsent != null) {
-            authorizedScopes = currentAuthorizationConsent.getScopes();
-        } else {
-            authorizedScopes = Collections.emptySet();
-        }
-        for (String requestedScope : StringUtils.delimitedListToStringArray(scope, " ")) {
-            if (OidcScopes.OPENID.equals(requestedScope)) {
+
+        Set<String> authorizedScopes =
+                currentAuthConsent != null? currentAuthConsent.getScopes(): Collections.emptySet();
+
+        for (String requestedScope : StringUtils.delimitedListToStringArray(scopes, " ")) {
+            // Skip openid scope as it is already approved by the user during the login process
+            if (OidcScopes.OPENID.equals(requestedScope))
                 continue;
-            }
             if (authorizedScopes.contains(requestedScope)) {
                 previouslyApprovedScopes.add(requestedScope);
             } else {
                 scopesToApprove.add(requestedScope);
             }
         }
+        Map<String, Set<String>> resultMap = new HashMap<>();
+        resultMap.put("scopesToApprove", scopesToApprove);
+        resultMap.put("previouslyApprovedScopes", previouslyApprovedScopes);
 
-        model.addAttribute("clientId", clientId);
-        model.addAttribute("clientName", registeredClient.getClientName());
-        model.addAttribute("state", state);
-        model.addAttribute("scopes", withDescription(scopesToApprove));
-        model.addAttribute("previouslyApprovedScopes", withDescription(previouslyApprovedScopes));
-        model.addAttribute("principalName", principal.getName());
-        model.addAttribute("userCode", userCode);
-        if (StringUtils.hasText(userCode)) {
-            model.addAttribute("requestURI", "/oauth2/device_verification");
-        } else {
-            model.addAttribute("requestURI", "/oauth2/authorize");
-        }
-
-        return "consent";
+        return resultMap;
     }
 
     private static Set<ScopeWithDescription> withDescription(Set<String> scopes) {
